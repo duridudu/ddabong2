73dca05ddbbce115df2185c1f6e785b253930bc1
Jihee <115629285+Anjihee@users.noreply.github.com>
2025-01-15 14:10:19 +0900

    Update CreateUserService.swift

--- Ddabong2/Ddabong2/API/CreateUserService.swift
+++ Ddabong2/Ddabong2/API/CreateUserService.swift
@@ -1,43 +1,26 @@
-//
-//  CreateUserService.swift
-//  Ddabong2
-//
-//  Created by 안지희 on 1/14/25.
-//
-
-import Foundation
+// CreateUserService.swift
 import Alamofire
 
 class CreateUserService {
     static let shared = CreateUserService()
     
     private init() {}
-    
-    func createUser(parameters: [String: Any], completion: @escaping (Result<String, Error>) -> Void) {
+
+    func createUser(request: UserCreateRequest, completion: @escaping (Result<UserCreateResponse, Error>) -> Void) {
         let url = "https://myhands.store/user/join"
         let headers: HTTPHeaders = [
-            "Authorization": "Bearer \(UserSessionManager.shared.getAccessToken() ?? "")"
+            "Authorization": "Bearer \(UserSessionManager.shared.getAccessToken() ?? "")",
+            "Content-Type": "application/json"
         ]
-        
-        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
-            .responseDecodable(of: ApiResponse.self) { response in
+
+        AF.request(url, method: .post, parameters: request, encoder: JSONParameterEncoder.default, headers: headers)
+            .responseDecodable(of: UserCreateResponse.self) { response in
                 switch response.result {
-                case .success(let apiResponse):
-                    if apiResponse.status == "CREATED" {
-                        completion(.success(apiResponse.message))
-                    } else {
-                        completion(.failure(NSError(domain: "", code: apiResponse.code ?? 0, userInfo: [NSLocalizedDescriptionKey: apiResponse.message])))
-                    }
+                case .success(let data):
+                    completion(.success(data))
                 case .failure(let error):
                     completion(.failure(error))
                 }
             }
     }
-}
-
-// API 응답 모델
-struct ApiResponse: Decodable {
-    let status: String
-    let code: Int?
-    let message: String
 }
